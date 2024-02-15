d4e63538e9f2361a4f461800ee9780c5ba07a1d7
unknown <amitbiswas9282@gmail.com>
2024-02-15 20:45:16 +0600

    This is Secend Comment

--- script/alga-clash.js
+++ script/alga-clash.js
@@ -11,6 +11,55 @@
 // }
 
 
+
+function handleKeybordKeyUpEvent(event){
+    const playerPress = event.key;
+    console.log('player pressed', playerPress);
+
+    // get the expected to press 
+    const currnetAlphabetElement = document.getElementById('currnet-alphabet');
+    const currnetAlphabet = currnetAlphabetElement.innerText;
+    const expectedElement = currnetAlphabet.toLocaleLowerCase();
+    // console.log(playerPress, expectedElement);
+
+    // cheak matched or not
+    if(playerPress === expectedElement){
+        console.log('You have pressed currectly', expectedElement);
+        // Update Score
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currnetScore = parseInt(currentScoreText);
+        console.log(currnetScore);
+        // 2. increase the score by 1 
+        const newScore = currnetScore + 1;
+        // 3. show the update score
+        currentScoreElement.innerText = newScore;
+        // start a new round 
+        console.log('you got a point');
+        removeBackgroundColorById(expectedElement);
+        continueGames();
+    }
+    else{
+        console.log('you missed. you loss a life');
+        // 1. get the current life number 
+        const currnetLifeElement = document.getElementById('currnet-life');
+        const currentLifeText = currnetLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // 2. reduce the life const 
+        const newLife = currentLife - 1;
+
+        // display the update life count
+        currnetLifeElement.innerText = newLife;
+
+    }
+}
+// capture keybord keypress
+document.addEventListener('keyup', handleKeybordKeyUpEvent);
+
+
+
 function continueGames(){
     // Setup 1: genarate a random alphabaet 
     const alphabet = getARandonAlphabet();
